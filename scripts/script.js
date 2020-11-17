d3f57d044aa410c58edb939172786d360869f92f
nicolasnomal <71435226+nicolasnomal@users.noreply.github.com>
2020-11-17 20:44:44 +0100

    premier push
    commentaires et indentation à faire

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+$('#menu').on('click',function(){
+
+    $('#menumodal').fadeIn(600);
+    $('#defaultmain').fadeOut(600);
+ 
+    $('#menu').fadeOut(200);
+    $('#close').fadeIn(200);
+ 
+});
+
+$('#close').on('click',function(){
+
+    $('#menumodal').fadeOut(600);    
+    $('#defaultmain').fadeIn(600);
+
+    $('#menu').fadeIn(200);
+    $('#close').fadeOut(200);
+   
+});
+
+$('#accept').on('click',function(){
+
+    $('#cookiepolicy').fadeOut(200);    
+  
+});
